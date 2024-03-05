8ed92946cc110bd6d092f2fe998008e40b7d28a4
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-05 20:20:04 -0300

    se comenzo a trabajar con vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  
-  <RouterView />
+  <v-card
+    elevation="3"
+    max-width="1200"
+    class="mx-auto"
+  >
+    <v-layout>
+      <v-app-bar
+        color="blue-darken-1"
+      >
+        <template v-slot:append>
+          
+        </template>
+
+        <template v-slot:prepend>
+          
+        </template>
+        
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+      
+    </v-layout>
+    
+  </v-card>
 </template>
