1d00d032311e48d6733f8402b034e29ac136998c
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-08 21:38:42 -0300

    Se creo un esquema de validacion para el formulario y se agrego la logica para mostrar los mensajes de error

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,5 +1,27 @@
 <script setup>
+    import {useForm, useField} from 'vee-validate';
+    import { validationSchema, imageSchema } from '@/validation/propiedadSchema';
+   
     const items = [1, 2, 3, 4, 5]
+
+    const { handleSubmit } = useForm({
+        validationSchema: {
+            ...validationSchema,
+            ...imageSchema
+        }
+    });
+
+    const titulo = useField('titulo');
+    const imagen = useField('imagen');
+    const precio = useField('precio');
+    const habitaciones = useField('habitaciones');
+    const wc = useField('wc');
+    const estacionamiento = useField('estacionamiento');
+    const descripcion = useField('descripcion');
+    
+    const submit = handleSubmit((values) => {
+        console.log(values);
+    })
 </script>
 
 <template>
@@ -22,6 +44,8 @@
         <v-text-field 
             class="mb-5"
             label="Titulo Propiedad"
+            v-model="titulo.value.value"
+            :error-messages="titulo.errorMessage.value"
         />
 
         <v-file-input
@@ -29,11 +53,15 @@
             label="Fotografía"
             prepend-icon="mdi-camera"
             class="mb-5"
+            v-model="imagen.value.value"
+            :error-messages="imagen.errorMessage.value"
         />
 
         <v-text-field 
             class="mb-5"
             label="Precio"
+            v-model="precio.value.value"
+            :error-messages="precio.errorMessage.value"
         />
 
         <v-row>
@@ -45,6 +73,8 @@
                     label="Habitaciones"
                     class="mb-5"
                     :items="items"
+                    v-model="habitaciones.value.value"
+                    :error-messages="habitaciones.errorMessage.value"
                 />
             </v-col>
 
@@ -56,6 +86,8 @@
                     label="WC"
                     class="mb-5"
                     :items="items"
+                    v-model="wc.value.value"
+                    :error-messages="wc.errorMessage.value"
                 />
             </v-col>
 
@@ -67,17 +99,25 @@
                     label="Lugares Estacionamiento"
                     class="mb-5"
                     :items="items"
+                    v-model="estacionamiento.value.value"
+                    :error-messages="estacionamiento.errorMessage.value"
                 />
             </v-col>
         </v-row>
 
-            <v-textarea class="mb-5" label="Descripción"></v-textarea>
+            <v-textarea 
+                class="mb-5" 
+                label="Descripción"
+                v-model="descripcion.value.value"
+                :error-messages="descripcion.errorMessage.value"
+            ></v-textarea>
 
             <v-checkbox label="Alberca"/>
 
             <v-btn
                 color="pink-accent-3"
                 block
+                @click="submit"
             >
                 Agregar propiedad
             </v-btn>
