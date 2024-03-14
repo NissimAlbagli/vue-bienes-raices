3c4bea4d22ef4153aff81c263c0fe6e7a69f7b70
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-14 02:35:26 -0300

    Se agrego la funcionalidad para eliminar propiedades e imagenes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
   import Propiedad from '../components/Propiedad.vue';
   import { propertyPrice } from '@/helpers';
 
-  const { alberca, propiedadesFiltradas } = usePropiedades();
+  const { alberca, filteredItems } = usePropiedades();
 </script>
 
 <template>
@@ -17,7 +17,7 @@
       </v-card-title>
       <v-row>
         <Propiedad 
-          v-for="propiedad in propiedadesFiltradas"
+          v-for="propiedad in filteredItems"
           :key="propiedad.id"
           :propiedad="propiedad"
           :price="propertyPrice"
